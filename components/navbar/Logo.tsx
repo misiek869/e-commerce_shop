0859a0dacc15db64bdea0058f6781c4907623f21
Michal Gawik <gawlik.zator@o2.pl>
2025-02-02 07:42:16 +0100

    create logo component

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { TiLeaf } from 'react-icons/ti'
+import { Button } from '../ui/button'
+import Link from 'next/link'
 
 const Logo = () => {
-	return <div>logo</div>
+	return (
+		<Button size={'icon'} asChild>
+			<Link href={'/'}>
+				<TiLeaf className='w-6 h-6' />
+			</Link>
+		</Button>
+	)
 }
 
 export default Logo
