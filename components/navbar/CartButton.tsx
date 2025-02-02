ec52351096b04a951f9b7d60bfe2c2edc61c095b
Michal Gawik <gawlik.zator@o2.pl>
2025-02-02 08:06:10 +0100

    create button and style

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import { FiShoppingCart } from 'react-icons/fi'
 
-const CartButton = () => {
-	return <div>CartButton</div>
+const CartButton = async () => {
+	const numberOfCartItems = 3
+
+	return (
+		<Button
+			asChild
+			variant={'outline'}
+			size={'icon'}
+			className='flex justify-center items-center relative'>
+			<Link href={'/cart'}>
+				<FiShoppingCart />
+				<span className='absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-sm'>
+					{numberOfCartItems}
+				</span>
+			</Link>
+		</Button>
+	)
 }
 
 export default CartButton
