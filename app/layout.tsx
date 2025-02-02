290ae359aa167401e8e2b49189da7ca298be86a3
Michal Gawik <gawlik.zator@o2.pl>
2025-02-02 07:27:08 +0100

    add navbar and container

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/navbar/Navbar'
+import Container from '@/components/global/Container'
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -26,7 +28,9 @@ export default function RootLayout({
 		<html lang='en'>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				{children}
+				<Navbar />
+
+				<Container>{children}</Container>
 			</body>
 		</html>
 	)
