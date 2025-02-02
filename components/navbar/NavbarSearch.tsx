fb039b7b2a2f2e7b957fe028baaf1468a6c258ca
Michal Gawik <gawlik.zator@o2.pl>
2025-02-02 07:52:12 +0100

    add input

--- components/navbar/NavbarSearch.tsx
+++ components/navbar/NavbarSearch.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Input } from '../ui/input'
 
 const NavbarSearch = () => {
-	return <>NavbarSearch</>
+	return (
+		<Input
+			className='max-w-xs dark:bg-muted'
+			type='search'
+			placeholder='search for product...'></Input>
+	)
 }
 
 export default NavbarSearch
