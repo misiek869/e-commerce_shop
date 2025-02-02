da7c5ac18786ed93356a30e26b0ef45ea8bfc251
Michal Gawik <gawlik.zator@o2.pl>
2025-02-02 07:26:52 +0100

    create nav layout

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Container from '../global/Container'
+import Logo from './Logo'
+import NavbarSearch from './NavbarSearch'
+import CartButton from './CartButton'
+import DarkMode from './DarkMode'
+import LinksDropdown from './LinksDropdown'
 
 const Navbar = () => {
-	return <div>navbar</div>
+	return (
+		<nav className='border-b'>
+			<Container className='flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4'>
+				<Logo />
+				<NavbarSearch />
+				<div className='flex-gap-r items-center'>
+					<CartButton />
+					<DarkMode />
+					<LinksDropdown />
+				</div>
+			</Container>
+		</nav>
+	)
 }
 
 export default Navbar
