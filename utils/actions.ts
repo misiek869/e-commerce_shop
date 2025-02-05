16fb419df416e8642b359bb4239b8944f2a01740
Michal Gawik <gawlik.zator@o2.pl>
2025-02-05 07:53:51 +0100

    fetchFeaturedProducts with prisma

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,17 +1,26 @@
 const url: string = 'https://e-commerce-api-it0a.onrender.com/'
+import db from '@/utils/db'
+// export const fetchFeaturedProducts = async () => {
+// 	try {
+// 		const response = await fetch(`${url}products?featured=true`)
+// 		if (!response.ok) {
+// 			throw new Error('Network response was not ok')
+// 		}
+// 		const products = await response.json()
+// 		return products
+// 	} catch (error) {
+// 		console.error('Error fetching featured products:', error)
+// 		return []
+// 	}
+// }
 
 export const fetchFeaturedProducts = async () => {
-	try {
-		const response = await fetch(`${url}products?featured=true`)
-		if (!response.ok) {
-			throw new Error('Network response was not ok')
-		}
-		const products = await response.json()
-		return products
-	} catch (error) {
-		console.error('Error fetching featured products:', error)
-		return []
-	}
+	const products = await db.product.findMany({
+		where: {
+			featured: true,
+		},
+	})
+	return products
 }
 
 export const fetchAllProducts = async () => {
