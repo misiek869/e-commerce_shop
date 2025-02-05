82a8b7abf0d3ff8cfb1bf5418b03bc0ac2969db1
Michal Gawik <gawlik.zator@o2.pl>
2025-02-05 08:30:15 +0100

    create grid

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { formatCurrency } from '@/utils/format'
+import { Product } from '@prisma/client'
+import Link from 'next/link'
+import { Card, CardContent } from '../ui/card'
 
-const ProductsGrid = () => {
-	return <div>ProductsGrid</div>
+const ProductsGrid = ({ products }: { products: Product[] }) => {
+	return (
+		<div className='pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
+			{products.map(product => {
+				const { name, price, image } = product
+				const productId = product.id
+
+				const dollars = formatCurrency(price)
+
+				return (
+					<article key={productId} className='group relative'>
+						<Link href={`product/${productId}`}>
+							<Card className='transform group-hover:shadow-xl transition-shadow duration-700'>
+								<CardContent className='p-4'>
+									<div className='relative h-64 md:h-48 rounded overflow-hidden'></div>
+								</CardContent>
+							</Card>
+						</Link>
+					</article>
+				)
+			})}
+		</div>
+	)
 }
 
 export default ProductsGrid
