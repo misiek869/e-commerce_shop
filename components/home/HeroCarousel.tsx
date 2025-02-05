30369cacc93a5afa8c2b483ee8578b30a16114c5
Michal Gawik <gawlik.zator@o2.pl>
2025-02-05 11:07:25 +0100

    create carousel

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from '@/components/ui/carousel'
+
+import hero1 from '@/public/images/hero1.jpg'
+import hero2 from '@/public/images/hero2.jpg'
+import hero3 from '@/public/images/hero3.jpg'
+
+const carouselImages = [hero1, hero2, hero3]
 
 const HeroCarousel = () => {
-	return <div>HeroCarousel</div>
+	return (
+		<div className='hidden lg:block'>
+			<Carousel>
+				<CarouselContent></CarouselContent>
+				<CarouselPrevious />
+				<CarouselNext />
+			</Carousel>
+		</div>
+	)
 }
 
 export default HeroCarousel
