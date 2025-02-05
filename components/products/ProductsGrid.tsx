d7cedcd317addd9c0bd90e32a9b96c46c876101e
Michal Gawik <gawlik.zator@o2.pl>
2025-02-05 08:36:11 +0100

    create grid

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -2,6 +2,7 @@ import { formatCurrency } from '@/utils/format'
 import { Product } from '@prisma/client'
 import Link from 'next/link'
 import { Card, CardContent } from '../ui/card'
+import Image from 'next/image'
 
 const ProductsGrid = ({ products }: { products: Product[] }) => {
 	return (
@@ -17,7 +18,15 @@ const ProductsGrid = ({ products }: { products: Product[] }) => {
 						<Link href={`product/${productId}`}>
 							<Card className='transform group-hover:shadow-xl transition-shadow duration-700'>
 								<CardContent className='p-4'>
-									<div className='relative h-64 md:h-48 rounded overflow-hidden'></div>
+									<div className='relative h-64 md:h-48 rounded overflow-hidden'>
+										<Image
+											src={image}
+											alt={name}
+											fill
+											sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw'
+											priority
+											className='rounded w-full object-cover transform group-hover:scale-110 transition-transform duration-500'></Image>
+									</div>
 								</CardContent>
 							</Card>
 						</Link>
