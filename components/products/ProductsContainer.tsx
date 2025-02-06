8c3db1ad79a7e4121817d7df3f682885721a1936
Michal Gawik <gawlik.zator@o2.pl>
2025-02-06 08:31:21 +0100

    create grid layout for products

--- components/products/ProductsContainer.tsx
+++ components/products/ProductsContainer.tsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import ProductsGrid from './ProductsGrid'
+import ProductsList from './ProductsList'
+import { LuLayoutGrid, LuList } from 'react-icons/lu'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { fetchAllProducts } from '@/utils/actions'
+import Link from 'next/link'
 
-const ProductsContainer = () => {
-	return <div>ProductsContainer</div>
+const ProductsContainer = async ({
+	layout,
+	search,
+}: {
+	layout: string
+	search: string
+}) => {
+	const products = await fetchAllProducts()
+
+	const totalProducts = products.length
+
+	const searchTerm = search ? `&search=${search}` : ''
+
+	return (
+		<>
+			<section>
+				<div className='flex justify-between items-center'>
+					<h4 className='font-medium text-lg'>
+						{totalProducts} product{totalProducts > 1 && 's'}
+					</h4>
+					<div className='flex gap-x-4 '>
+						<Button
+							variant={layout === 'grid' ? 'default' : 'ghost'}
+							size={'icon'}
+							asChild>
+							<Link href={`/products?layout=grid${searchTerm}`}>
+								<LuLayoutGrid />
+							</Link>
+						</Button>
+						<Button
+							variant={layout === 'list' ? 'default' : 'ghost'}
+							size={'icon'}
+							asChild>
+							<Link href={`/products?layout=list${searchTerm}`}>
+								<LuList />
+							</Link>
+						</Button>
+					</div>
+				</div>
+				<Separator className='mt-4' />
+			</section>
+			<div>
+				{totalProducts === 0 ? (
+					<h5 className='text-2xl mt-16'>
+						We don't have what you're looking for
+					</h5>
+				) : layout === 'grid' ? (
+					<ProductsGrid products={products} />
+				) : (
+					<ProductsList products={products} />
+				)}
+			</div>
+		</>
+	)
 }
 
 export default ProductsContainer
--- components/products/ProductsList.tsx
+++ components/products/ProductsList.tsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import { formatCurrency } from '@/utils/format'
+import Link from 'next/link'
+import { Card, CardContent } from '@/components/ui/card'
+import { Product } from '@prisma/client'
+import Image from 'next/image'
+import FavoriteToggleButton from './FavoriteToggleButton'
 
-const ProductsList = () => {
-	return <div>ProductsList</div>
+const ProductsList = ({ products }: { products: Product[] }) => {
+	return (
+		<div className='mt-12 grid gap-y-8'>
+			{products.map(product => {
+				const { name, price, image, company } = product
+				const productId = product.id
+
+				const dollars = formatCurrency(price)
+
+				return (
+					<article key={productId} className='group relative'>
+						<Link href={`product/${productId}`}>
+							<Card className='transform group-hover:shadow-xl transition-shadow duration-700'>
+								<CardContent className='p-4 gap-y-4 grid md:grid-cols-3'>
+									<div className='relative h-64 md:h-48 '>
+										<Image
+											src={image}
+											alt={name}
+											fill
+											sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw'
+											priority
+											className='rounded w-full object-cover'></Image>
+									</div>
+									<div className='ml-16'>
+										<h2 className='text-xl capitalize'>{name}</h2>
+										<h4 className='text-muted-foreground capitalize'>
+											{company}
+										</h4>
+									</div>
+									<p className='text-muted-foreground text-lg md:ml-auto'>
+										{dollars}
+									</p>
+								</CardContent>
+							</Card>
+						</Link>
+						<div className='absolute bottom-7 right-7 z-50'>
+							<FavoriteToggleButton productId={productId} />
+						</div>
+					</article>
+				)
+			})}
+		</div>
+	)
 }
 
 export default ProductsList
