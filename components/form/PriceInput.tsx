cc09010fedab961d3388d71ec2dde5d88f23f18c
Michal Gawik <gawlik.zator@o2.pl>
2025-02-11 11:58:08 +0100

    create PriceInput body

--- components/form/PriceInput.tsx
+++ components/form/PriceInput.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
 
-const PriceInput = () => {
-	return <div></div>
+const name = 'price'
+
+type PriceInputProps = {
+	defaultValue?: number
+}
+
+const PriceInput = ({ defaultValue }: PriceInputProps) => {
+	return (
+		<div className='mb-2'>
+			<Label htmlFor={name} className='capitalize'>
+				Price ($)
+			</Label>
+			<Input
+				id={name}
+				type='number'
+				name={name}
+				required
+				min={0}
+				defaultValue={defaultValue || 100}
+			/>
+		</div>
+	)
 }
 
 export default PriceInput
