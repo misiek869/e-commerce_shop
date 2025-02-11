65b8870a728308354ad5a26a629df492b209dcc3
Michal Gawik <gawlik.zator@o2.pl>
2025-02-11 12:57:46 +0100

    submit btn complete

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,4 +1,46 @@
-import React from 'react'
+'use client'
+
+import { TfiReload } from 'react-icons/tfi'
+import { useFormStatus } from 'react-dom'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { SignInButton } from '@clerk/nextjs'
+import { CiHeart } from 'react-icons/ci'
+import { IoHeartSharp } from 'react-icons/io5'
+import { LuTrash2, LuPenSquare } from 'react-icons/lu'
+
+type buttonSize = 'default' | 'lg' | 'sm'
+
+type SubmitButtonProps = {
+	className?: string
+	text?: string
+	size?: btnSize
+}
+
+export const SubmitButton = ({
+	className = '',
+	text = 'submit',
+	size = 'lg',
+}: SubmitButtonProps) => {
+	const { pending } = useFormStatus()
+
+	return (
+		<Button
+			type='submit'
+			disabled={pending}
+			className={cn('capitalize', className)}
+			size={size}>
+			{pending ? (
+				<>
+					<TfiReload className='mr-2 h-4 w-4 animate-spin' />
+					Wait...
+				</>
+			) : (
+				text
+			)}
+		</Button>
+	)
+}
 
 const Buttons = () => {
 	return <div></div>
--- components/form/CheckboxInput.tsx
+++ components/form/CheckboxInput.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+'use client'
 
-const CheckboxInput = () => {
-	return <div></div>
+import { Checkbox } from '@/components/ui/checkbox'
+
+type CheckboxInputProps = {
+	name: string
+	label: string
+	defaultChecked?: boolean
+}
+
+const CheckboxInput = ({
+	name,
+	label,
+	defaultChecked = false,
+}: CheckboxInputProps) => {
+	return (
+		<div className='flex items-center space-x-2'>
+			<Checkbox id={name} name={name} defaultChecked={defaultChecked} />
+			<label
+				htmlFor={name}
+				className='text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 capitalize'>
+				{label}
+			</label>
+		</div>
+	)
 }
 
 export default CheckboxInput
