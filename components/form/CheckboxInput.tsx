7ceb88585b6532929a40c32e08b0cda54af80803
Michal Gawik <gawlik.zator@o2.pl>
2025-02-11 11:32:19 +0100

    create

---
+++ components/form/CheckboxInput.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CheckboxInput = () => {
+	return <div></div>
+}
+
+export default CheckboxInput
---
+++ components/form/PriceInput.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PriceInput = () => {
+	return <div></div>
+}
+
+export default PriceInput
