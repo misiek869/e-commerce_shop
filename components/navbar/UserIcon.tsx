c2a1a2493d83ce72630c8db32223255fb1a5df36
Michal Gawik <gawlik.zator@o2.pl>
2025-02-11 09:32:40 +0100

    create component body

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { CiUser } from 'react-icons/ci'
+import { currentUser } from '@clerk/nextjs/server'
 
-const UserIcon = () => {
-	return <div>UserIcon</div>
+const UserIcon = async () => {
+	const user = await currentUser()
+
+	const profileImage = user?.imageUrl
+
+	if (profileImage) {
+		return (
+			<img
+				src={profileImage}
+				alt='profile image'
+				className='w-6 h-6 rounded-full object-cover'
+			/>
+		)
+	}
+
+	return <CiUser className='w-6 h-6 rounded-full text-white' />
 }
 
 export default UserIcon
