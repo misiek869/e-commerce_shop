4b289586fe653781514ce09e0bb656e495632bcb
Michal Gawik <gawlik.zator@o2.pl>
2025-02-11 10:18:15 +0100

    create layout

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+'use client'
+import { adminLinks } from '@/utils/links'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { Button } from '@/components/ui/button'
 
 const Sidebar = () => {
-	return <div></div>
+	const pathName = usePathname()
+
+	return (
+		<aside>
+			{adminLinks.map((link, index) => {
+				const isPageActive = pathName === link.href
+				const pageVariant = isPageActive ? 'secondary' : 'ghost'
+
+				return (
+					<Button
+						asChild
+						key={index}
+						className='w-full mb-2 capitalize font-normal'
+						variant={pageVariant}>
+						<Link href={link.href}>{link.label}</Link>
+					</Button>
+				)
+			})}
+		</aside>
+	)
 }
 
 export default Sidebar
--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,20 @@
+import { Separator } from '@/components/ui/separator'
+import Sidebar from './Sidebar'
 import React from 'react'
 
-const layout = () => {
-	return <div></div>
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<>
+			<h2 className='text-2xl pl-4 mb-2'>Dashboard</h2>
+			<Separator />
+			<section className='grid lg:grid-cols-12 gap-12 mt-12'>
+				<div className='lg:col-span-2'>
+					<Sidebar />
+				</div>
+				<div className='lg:col-span-10 px-4'>{children}</div>
+			</section>
+		</>
+	)
 }
 
-export default layout
+export default AdminLayout
