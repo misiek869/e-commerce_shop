73c443a24ee8b53836975d487eb2bda22797d160
Michal Gawik <gawlik.zator@o2.pl>
2025-02-12 05:30:33 +0100

    create category input

---
+++ components/form/CategoryInput.tsx
@@ -0,0 +1,32 @@
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select'
+
+const CategoryInput = () => {
+	return (
+		<div className='mb-2'>
+			<Select>
+				<SelectTrigger>
+					<SelectValue placeholder='Select a company' />
+				</SelectTrigger>
+				<SelectContent>
+					<SelectGroup>
+						<SelectItem value='ikea'>Ikea</SelectItem>
+						<SelectItem value='liddy'>Liddy</SelectItem>
+						<SelectItem value='marcos'>Marcos</SelectItem>
+					</SelectGroup>
+				</SelectContent>
+			</Select>
+		</div>
+	)
+}
+
+export default CategoryInput
